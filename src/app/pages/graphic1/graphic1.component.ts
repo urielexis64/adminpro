f1c515a17577488a85adfcd5790358d336c5c1fa
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-27 20:36:36 -0700

    Graphic component

--- src/app/pages/graphic1/graphic1.component.ts
+++ src/app/pages/graphic1/graphic1.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-graphic1',
   templateUrl: './graphic1.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class Graphic1Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class Graphic1Component {
+  public labels1: string[] = ['Download Sales', 'In-order Sales', 'Others'];
+  public data1 = [[123, 321, 356]];
+  public colors1: Color[] = [
+    { backgroundColor: ['#6857e6', '#009fee', '#f02059'] },
+  ];
+  public labels2: string[] = [
+    'Download Sales',
+    'In-order Sales',
+    'Best',
+    'Others',
+  ];
+  public data2 = [[765, 87, 111, 66]];
+  public colors2: Color[] = [
+    { backgroundColor: ['#a34718', '#fe4466', '#9783ae'] },
+  ];
 }
