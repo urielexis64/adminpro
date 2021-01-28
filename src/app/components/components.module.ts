d0af04f6a371cc0e6be6d2492d36e3db9b6c7de5
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-27 20:15:09 -0700

    Dognut Component & Charts module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IncreaserComponent } from './increaser/increaser.component';
 import { FormsModule } from '@angular/forms';
+import { DognutComponent } from './dognut/dognut.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
-  declarations: [IncreaserComponent],
-  imports: [CommonModule, FormsModule],
-  exports: [IncreaserComponent],
+  declarations: [IncreaserComponent, DognutComponent],
+  imports: [CommonModule, FormsModule, ChartsModule],
+  exports: [IncreaserComponent, DognutComponent],
 })
 export class ComponentsModule {}
