486e1ccbf11e9ea4ed62cc1a3b24e700ddb7b55a
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-27 20:18:30 -0700

    Reordered imports

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graphic1Component } from './graphic1/graphic1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
-import { FormsModule } from '@angular/forms';
-import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   declarations: [
@@ -20,9 +20,9 @@ import { ComponentsModule } from '../components/components.module';
   ],
   imports: [
     CommonModule,
-    SharedModule,
     RouterModule,
     FormsModule,
+    SharedModule,
     ComponentsModule,
   ],
   exports: [
