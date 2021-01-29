a5ac1a5f5ec5f7ea33928726daa91aa89029f631
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-28 20:25:16 -0700

    settings service injected, customInit Functions called on Init

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
 
+declare function customInitFunctions();
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PagesComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _settingsService: SettingsService) {}
 
   ngOnInit(): void {
+    customInitFunctions();
   }
-
 }
