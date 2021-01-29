4662c3f6c114fd491d9432b933daba2e5afac71b
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-28 20:23:56 -0700

    Router & Forms modules imported

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   exports: [LoginComponent, RegisterComponent],
 })
 export class AuthModule {}
